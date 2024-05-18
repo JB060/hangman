ec90830c841ac40023afa5e15c0c2db44eb0c069
Jason Byrne <jasonbyrne060@gmail.com>
2024-05-18 16:45:52 +0000

    added the hang man figure to the app to allow users to see the their at in the game

--- run.py
+++ run.py
@@ -1,3 +1,68 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+print ("welcome to Hangman")
+print ("--------------------------------------------")
+
+wordDictionary +["graze""tooth""paper""warning""presentation""nuclear""parking""performance""surgeon""trust""denial"
+"bathroom""campaign""equation""swipe""discourage""wine""image""snail""management""tray""trail""studio""south"]
+
+### choose a random word 
+randomWord = random.choice(wordDictionary)
+
+
+for x in randomWord:
+    print ("_", end=" ")
+
+def print_hangman(wrong):
+    if (wrong == 0):
+        print ("/n+---+")
+        print ("    |")
+        print ("    |")
+        print ("    |")
+        print ("   ===")
+     elif(wrong == 1):
+          print ("/n+---+")
+        print ("O   |")
+        print ("    |")
+        print ("    |")
+        print ("   ===")
+     elif(wrong == 2):
+        print ("/n+---+")
+        print ("O   |")
+        print ("|   |")
+        print ("    |")
+        print ("   ===")
+     elif(wrong == 3):
+        print ("/n+---+")
+        print (" O   |")
+        print ("/|   |")
+        print ("     |")
+        print ("   ===")
+     elif(wrong == 4):
+        print ("/n+---+")
+        print (" O   |")
+        print ("/|\  |")
+        print ("     |")
+        print ("   ===")
+        elif(wrong == 5):
+            print ("/n+---+")
+        print (" O   |")
+        print ("/|\  |")
+        print (" |   |")
+        print ("   ===")
+     elif(wrong == 6):
+        print ("/n+---+")
+        print (" O   |")
+        print ("/|\  |")
+        print ("/    |")
+        print ("   ===")
+     elif (wrong ==7):
+        print ("/n+---+")
+        print (" O    |")
+        print ("/|\   |")
+        print ("/ \   |")
+        print ("   ===")
+
+   
+    
+
